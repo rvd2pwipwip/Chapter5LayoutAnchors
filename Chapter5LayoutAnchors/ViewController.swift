5beb5b915a7b7825db7fc2426b365096dc1a4fc5
Herve Desrosiers <hdesrosiers@stingray.com>
2020-03-03 10:54:37 -0500

    initial commit

--- Chapter5LayoutAnchors/ViewController.swift
+++ Chapter5LayoutAnchors/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let padding: CGFloat = 50
+    private let spacing: CGFloat = 25
+    private let redView = UIView.makeView(color: .red)
+    private let greenView = UIView.makeView(color: .green)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupView()
     }
 
-
+    private func setupView() {
+        view.addSubview(redView)
+        view.addSubview(greenView)
+        NSLayoutConstraint.activate([
+            redView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: padding),
+            redView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: padding),
+            redView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -padding),
+            redView.heightAnchor.constraint(equalTo: greenView.heightAnchor),
+            greenView.topAnchor.constraint(equalTo: redView.bottomAnchor, constant: spacing),
+            greenView.leadingAnchor.constraint(equalTo: redView.leadingAnchor),
+            greenView.trailingAnchor.constraint(equalTo: redView.trailingAnchor),
+            greenView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -padding)
+        ])
+    }
 }
 
+private extension UIView {
+    static func makeView(color: UIColor) -> UIView {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = color
+        return view
+    }
+}
